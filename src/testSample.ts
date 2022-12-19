566009d6d1564deee2ac2012e7972187ecf5467a
Tom Sherkness <thomas.sherkness@envestnet.com>
2022-12-19 16:51:41 -0500

    add manual screenshot

--- src/testSample.ts
+++ src/testSample.ts
@@ -4,5 +4,6 @@ fixture('sample fixture')
     .page `https://devexpress.github.io/testcafe/example/`
 
 test('testcafe sample test', async t => {
+  await t.takeScreenshot()
   await t.expect(Selector('h1').withText('Exampla').exists).ok()
 })
