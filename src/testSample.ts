817b6bbf77aba9d1727627f7076a7ad9ccc85632
Tom Sherkness <thomas.sherkness@envestnet.com>
2022-12-19 16:58:49 -0500

    add comment for context

--- src/testSample.ts
+++ src/testSample.ts
@@ -4,6 +4,6 @@ fixture('sample fixture')
     .page `https://devexpress.github.io/testcafe/example/`
 
 test('testcafe sample test', async t => {
-  await t.takeScreenshot()
-  await t.expect(Selector('h1').withText('Exampla').exists).ok()
+  await t.takeScreenshot() //fails with odd error on node 18.2.1, works on node 14.17.6
+  await t.expect(Selector('h1').withText('Exampla').exists).ok() //stalls if taking screenshot on failure is enabled on node 18.2.1, works as expected on node 14.17.6
 })
